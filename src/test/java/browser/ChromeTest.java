7c15feba0a3693da9c186e0040791591d2cd4615
Thao Tran <thao.tran0510@outlook.com>
2025-07-12 22:55:24 +0700

    first commit

---
+++ src/test/java/browser/ChromeTest.java
@@ -0,0 +1,18 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ChromeTest {
+    @Test
+    void OpenWithDefaultMode(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+}
