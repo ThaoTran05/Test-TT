ae6f6fd75df9dfcc202e8d02de3f6bd4d980d4c3
Thao Tran <thao.tran0510@outlook.com>
2025-07-13 12:45:32 +0700

    update code chrome browser open with mode headless

--- src/test/java/browser/ChromeTest.java
+++ src/test/java/browser/ChromeTest.java
@@ -2,6 +2,7 @@ package browser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,6 +12,17 @@ public class ChromeTest {
     @Test
     void OpenWithDefaultMode(){
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+
+    @Test
+    void openBrowserWithHeadlessMode(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.selenium.dev/");
         Assert.assertEquals(driver.getTitle(),"Selenium");
         driver.quit();
